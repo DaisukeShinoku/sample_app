7d83dc5340321302d485ab336bed789644514daf
Daisuke Shinoku <shinokudaisuke@DaisukenoAir.lan>
2020-04-16 16:50:53 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cb445609fe66c2e594682528d5b8241f27f2cd47dc1362a71db7aebdd653c99294c7fa7ebad28268f101c82273b86d69d9e37d1a4d327f09d8a322188bfe4de7'
